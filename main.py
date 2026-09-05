e6eb694c76c6947042a417f8241ef7854231259f
Tsutsumi Kazunori <myrecruit11@gmail.com>
2026-09-05 15:01:52 +0900

    modify log

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def main():
             notification_type
         )
 
-        logging.info(f"{notification_type}: 対象0件")
-        print(f"{notification_type}: 対象0件")
+        logging.info(f"{notification_type}: {len(rows)}件")
+        print(f"{notification_type}: {len(rows)}件")
 
         if not rows:
             continue
